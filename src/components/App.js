a315ddcbea9426e449dc6ddf440832feb9c35170
Anders Bando-Hess <andersbando-hess@Anderss-MacBook-Pro.local>
2017-11-03 18:35:53 +0100

    boilerplate set, ready to begin dynamic ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import vis from 'vis';
-import { Loader } from 'semantic-ui-react';
+import React from 'react'
+import vis from 'vis'
+import wiki from 'wikijs'
+import { Loader } from 'semantic-ui-react'
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {loading: false};
+    this.nodes = new vis.DataSet([{id: 'Tim', label: 'Tim Tregubov', fixed: true}]);
+    this.edges = new vis.DataSet([]);
+  }
 
+  componentDidMount() {
+    const options = {
+      edges:{
+        arrows: 'to',
+        smooth: true,
+        width: 1
+      },
+      nodes: {
+        color: '#FFFFFF',
+        font: {
+          face: 'Lato'
+        },
+        shadow: true,
+        shape: 'box',
+      }
+    };
 
+    const container = document.getElementById('daliEmpoyees');
+    const network = new vis.Network(
+      container,
+      {nodes: this.nodes, edges: this.edges},
+      options
+    );
+
+    network.on("doubleClick", (params) => {
+      if (params.nodes.length > 0) {
+        this.addMember(params.nodes[0])
+      }
+    });
+
+  }
+
+  addMember(fromNodeTitle) {
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          id="daliEmpoyees"
+          style={{width: window.innerWidth, height: window.innerHeight}}/>
+        <Loader active={this.state.loading} style={{position: 'absolute', top:25}}/>
+      </div>
+    );
+  }
+
+}
 
 export default App;
