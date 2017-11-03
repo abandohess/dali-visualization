a1ee947e4fb866de69b2de67a75ab0ae23ffa3ec
Anders Bando-Hess <andersbando-hess@Anderss-MacBook-Pro.local>
2017-11-03 19:08:30 +0100

    incorporated pictures into network and loaded JSON object

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,17 @@ import { Loader } from 'semantic-ui-react'
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {loading: false};
-    this.nodes = new vis.DataSet([{id: 'Tim', label: 'Tim Tregubov', fixed: true}]);
+    this.state = {  loading: false };
+    this.nodes = new vis.DataSet([{id: 'Tim', label: 'Tim Tregubov', fixed: true, image:'http://mappy.dali.dartmouth.edu/images/tim_round.jpg'}]);
     this.edges = new vis.DataSet([]);
+    this.imgDir =  'http://mappy.dali.dartmouth.edu/';
+    this.employees = null;
   }
 
   componentDidMount() {
+    let requestURL = 'http://mappy.dali.dartmouth.edu/members.json';
+    let request = new XMLHttpRequest();
+    this.loadJSON();
     const options = {
       edges:{
         arrows: 'to',
@@ -19,12 +24,12 @@ class App extends React.Component {
         width: 1
       },
       nodes: {
+        shape: 'circularImage',
         color: '#FFFFFF',
         font: {
           face: 'Lato'
         },
         shadow: true,
-        shape: 'box',
       }
     };
 
@@ -37,13 +42,24 @@ class App extends React.Component {
 
     network.on("doubleClick", (params) => {
       if (params.nodes.length > 0) {
-        this.addMember(params.nodes[0])
+        console.log(this.employees[0]);
+        // this.addMember(params.nodes[0]);
       }
     });
 
   }
 
   addMember(fromNodeTitle) {
+  }
+
+  loadJSON() {
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+      this.employees = request.response;
+      console.log(this.employees[0]);
+    }
   }
 
   render() {
